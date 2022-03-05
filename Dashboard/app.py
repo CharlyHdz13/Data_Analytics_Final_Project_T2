346330a5036e4825cb1279bf1a452ca96a944c9a
unknown <charly97hernandez@gmail.com>
2022-03-05 14:40:00 -0600

    Dashboard's graphs

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,8 +1,32 @@
 # Import Flask to build web app
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for,jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, inspect, MetaData, Table
+
+from config import db_password
+
+import pandas as pd
+
+import json
+
 
 # Setup Flask
 app = Flask(__name__)
+
+# Set path to the database
+db_string = f"postgresql://postgres:{db_password}@data-analytics-final-project-t2.ceptpmc0vrha.us-east-1.rds.amazonaws.com/Medical_Data"
+
+# Create enginge to connect with db
+engine = create_engine(db_string)
+
+# Create inspector to inspect db
+inspector = inspect(engine)
+
+# Get the db as a df and save it as a json file
+patients_df = pd.read_sql_table('patients_clusters',db_string, index_col='identification')
+patients_df.to_json('patients.json')
 
 # Define route for main page of our dashboard
 @app.route("/")
@@ -24,6 +48,12 @@ def random_patient():
 @app.route("/predict")
 def predict():
    return redirect("/random-patient",code=302)
+
+@app.route("/patients.json")
+def patients():
+   with open('patients.json') as json_file:
+    data = json.load(json_file)
+   return data
 
 if __name__ == "__main__":
    app.run()
